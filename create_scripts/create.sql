5906ed0c1f51b078918bce299f9ad87aad47c4a9
Georgy Guminov <gogagum@gmail.com>
2020-04-07 23:10:24 +0300

    Scripts are ready and seem to be OK.

--- create_scripts/create.sql
+++ create_scripts/create.sql
@@ -4,12 +4,6 @@ create schema photographic_equipment;
 --############################ CREATING TABLES #############################
 --##########################################################################
 
-create table photographic_equipment.PRODUCTS
-(
-    product_id   integer primary key,
-    manufacturer varchar(20) not null,
-    model_nm     varchar(20) not null
-);
 
 create table photographic_equipment.MANUFACTURERS
 (
@@ -17,23 +11,73 @@ create table photographic_equipment.MANUFACTURERS
   foundation_dt   date
 );
 
-create table photographic_equipment.AWARDS
+create table photographic_equipment.PRODUCTS
 (
-  model_id integer NOT NULL,
-  title_id integer NOT NULL,
-  award_dt date
+    product_id   integer primary key,
+    manufacturer varchar(20) not null references photographic_equipment.MANUFACTURERS,
+    model_nm     varchar(20) not null
+);
+
+create table photographic_equipment.MOUNT_SUPPORT
+(
+    camera_mount_type varchar(10) not null,
+    lens_mount_type   varchar(10) not null,
+    PRIMARY KEY       (camera_mount_type, lens_mount_type)
 );
 
 create table photographic_equipment.AWARD_TITLES
 (
-  title_id        integer NOT NULL,
-  award_title_txt text    NOT NULL,
+  title_id        integer primary key ,
+  award_title_txt text    not null,
   first_given_dt  date
 );
 
-create table photographic_equipment.MATRIX_TYPE
+create table photographic_equipment.AWARDS
 (
-    matrix_type_nm varchar(10) primary key,
-    height         decimal(4, 2) not null,
-    width          decimal(4, 2) not null
+  model_id integer NOT NULL references photographic_equipment.PRODUCTS,
+  title_id integer NOT NULL references photographic_equipment.AWARD_TITLES,
+  award_dt date
 );
+
+create table photographic_equipment.PRODUCTION_PERIODS
+(
+  period_id           integer primary key,
+  model_id            integer NOT NULL    references photographic_equipment.PRODUCTS,
+  production_start_dt date    NOT NULL,
+  production_end_dt   date    check(production_end_dt > production_start_dt),
+  factory_nm          varchar(10)
+);
+
+create table photographic_equipment.MATRIX_TYPES
+(
+  matrix_type_nm varchar(10) primary key,
+  height         decimal(4, 2) not null,
+  width          decimal(4, 2) not null
+);
+
+create  table photographic_equipment.CAMERAS
+(
+  camera_id      integer     primary key references photographic_equipment.PRODUCTS,
+  matrix_type    varchar(10) not null    references photographic_equipment.MATRIX_TYPES,
+  has_flashlight boolean     not null,
+  mount_type     varchar(10) not null
+);
+
+create table photographic_equipment.LENSES
+(
+  lens_id               integer       primary key references photographic_equipment.PRODUCTS,
+  lens_mount_type       varchar(10)   not null,
+  focal_length          decimal(5, 2) not null    check (focal_length > 0),
+  min_aperture          decimal(5, 2) not null    check (min_aperture > 0),
+  max_aperture          decimal(5, 2) not null    check (max_aperture >= min_aperture),
+  diaphragm_blades_cnt  integer       not null    check (diaphragm_blades_cnt >= 0),
+  min_focusing_distance decimal(5, 2) not null    check (min_focusing_distance > 0),
+  filter_size           decimal(4, 1) not null    check (filter_size > 0),
+  has_autofocus         boolean       not null,
+  has_stabilization     boolean       not null
+);
+
+
+
+
+drop schema photographic_equipment cascade;
