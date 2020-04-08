3fed064e998a517c2575994695c5cba24cad43d8
Georgy Guminov <gogagum@gmail.com>
2020-04-09 01:38:26 +0300

    I am running out of my battery.

---
+++ filling_scripts/fill.sql
@@ -0,0 +1,14 @@
+insert into photographic_equipment.matrix_types (matrix_type_nm, width, height) VALUES
+('APS-C',       23.6, 15.8),
+('4/3',         17.3, 13.0),
+('APS-H Canon', 28.1, 18.7),
+('APS-H Leica', 27,   18  ),
+('APS-C Canon', 22.3, 14.9),
+('Foveon X3',   20.7, 13.8),
+('1\,5\"',      18.7, 14  ),
+('1\"',         13.2, 8.8 )
+
+insert into photographic_equipment.mount_support (camera_mount_type, lens_mount_type) VALUES
+('SONY FE', 'SONY FE' ),
+('SONY FE', 'SONY E' ),
+('SONY E',  'SONY E')
