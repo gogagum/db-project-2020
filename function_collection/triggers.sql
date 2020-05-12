da63ce95ea0e9a2e8424fd3a1ff2fdbbb600c8b2
Georgy Guminov <gogagum@gmail.com>
2020-05-13 01:00:48 +0300

    Second trigger. Say thanks to myself for previous work.

--- function_collection/triggers.sql
+++ function_collection/triggers.sql
@@ -1,4 +1,9 @@
-create or replace function photographic_equipment.delete_camera_data_() returns trigger as
+--########
+--## №1 ##
+--########
+
+--Функция для триггера
+create or replace function photographic_equipment.delete_camera_() returns trigger as
 $$
 BEGIN
   delete
@@ -19,10 +24,11 @@ $$ language plpgsql;
 
 
 --Триггер
+--В добавок к удалению данных о камере удаляет все связанные с данной камерой записи
 create trigger delete_camera after delete
     on photographic_equipment.cameras
 for each ROW
-    EXECUTE procedure photographic_equipment.delete_camera_data_();
+    EXECUTE procedure photographic_equipment.delete_camera_();
 
 --Если его надо удалить
 drop trigger delete_camera on photographic_equipment.cameras;
@@ -51,3 +57,39 @@ insert into photographic_equipment.production_periods (model_id, production_star
 delete
 from photographic_equipment.CAMERAS as CAMERAS
 where CAMERAS.camera_id = 666;
+
+--########
+--## №2 ##
+--########
+
+--Функция к триггеру
+create function photographic_equipment.check_awards_update_() returns trigger AS $$
+BEGIN
+    if NEW.award_dt < any (
+        select min(PRODUCTION_PERIODS_.production_start_dt) as production_start_dt
+        from
+             photographic_equipment.PRODUCTS as PRODUCTS_
+                 left join
+             photographic_equipment.PRODUCTION_PERIODS as PRODUCTION_PERIODS_
+             on PRODUCTS_.product_id = PRODUCTION_PERIODS_.model_id
+        where PRODUCTS_.product_id = NEW.model_id
+    )
+    then RAISE EXCEPTION 'award is given before the start_of_production';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+--Сам триггер
+create trigger check_awards_update
+    before insert or update
+    on photographic_equipment.Awards
+    for each ROW
+    execute procedure photographic_equipment.check_awards_update_();
+
+--Проверка триггера
+--Так как камера в тот момент ещё не производилась, то вставка не призойдёт
+insert into photographic_equipment.Awards(model_id, title_id, award_dt)
+values (00002, 1, '2007-09-01')
+;
